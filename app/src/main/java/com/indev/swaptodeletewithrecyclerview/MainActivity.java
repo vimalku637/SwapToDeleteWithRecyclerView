68d6b13302dcbd013751928e69beb92daec99246
Vimal Kumar <vimalku637@gmail.com>
2021-08-04 09:05:46 +0530

    initial commit

---
+++ app/src/main/java/com/indev/swaptodeletewithrecyclerview/MainActivity.java
@@ -0,0 +1,79 @@
+package com.indev.swaptodeletewithrecyclerview;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView rvSwapToDelete;
+    RecyclerViewAdapter mAdapter;
+    ArrayList<String> stringArrayList = new ArrayList<>();
+    CoordinatorLayout coordinatorLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvSwapToDelete = findViewById(R.id.rvSwapToDelete);
+        coordinatorLayout = findViewById(R.id.coordinatorLayout);
+
+        populateRecyclerView();
+        enableSwipeToDeleteAndUndo();
+    }
+
+    private void populateRecyclerView() {
+        stringArrayList.add("Item 1");
+        stringArrayList.add("Item 2");
+        stringArrayList.add("Item 3");
+        stringArrayList.add("Item 4");
+        stringArrayList.add("Item 5");
+        stringArrayList.add("Item 6");
+        stringArrayList.add("Item 7");
+        stringArrayList.add("Item 8");
+        stringArrayList.add("Item 9");
+        stringArrayList.add("Item 10");
+
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        rvSwapToDelete.setLayoutManager(mLayoutManager);
+        mAdapter = new RecyclerViewAdapter(stringArrayList);
+        rvSwapToDelete.setAdapter(mAdapter);
+    }
+
+    private void enableSwipeToDeleteAndUndo() {
+        SwipeToDeleteCallback swipeToDeleteCallback = new SwipeToDeleteCallback(this) {
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+                final int position = viewHolder.getAdapterPosition();
+                final String item = mAdapter.getData().get(position);
+
+                mAdapter.removeItem(position);
+                Snackbar snackbar = Snackbar
+                        .make(coordinatorLayout, "Item was removed from the list.", Snackbar.LENGTH_LONG);
+                snackbar.setAction("UNDO", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mAdapter.restoreItem(item, position);
+                        rvSwapToDelete.scrollToPosition(position);
+                    }
+                });
+
+                snackbar.setActionTextColor(Color.YELLOW);
+                snackbar.show();
+            }
+        };
+        ItemTouchHelper itemTouchhelper = new ItemTouchHelper(swipeToDeleteCallback);
+        itemTouchhelper.attachToRecyclerView(rvSwapToDelete);
+    }
+}
